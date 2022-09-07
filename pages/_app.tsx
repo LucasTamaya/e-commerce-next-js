fd76ce73e46d1e0fa8783bcdef638dfdfc2d4c08
LucasTamaya <lucastamaya.974@gmail.com>
2022-09-07 08:25:27 +0400

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Next-Ecommerce</title>
+        <meta
+          name="description"
+          content="An e-commerce web app using some fresh technologies such as Next JS Tailwind and much more !"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <h1 className="text-red-500">Welcome to Next JS app</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
